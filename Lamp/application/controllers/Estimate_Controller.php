008cc4645fb82be061a1e418cc1c28b43916042f
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2021-05-31 11:12:41 -0700

    calculations incorporated

--- Lamp/application/controllers/Estimate_Controller.php
+++ Lamp/application/controllers/Estimate_Controller.php
@@ -67,11 +67,12 @@ class Estimate_Controller extends CI_Controller {
             $weight = $this->session->userdata('estimate')['weight'];
             
             // Calculations
-            $necropsy_cost = number_format($weight * 2.3, 2, '.', '');
-            $cremation_cost = number_format($weight + 10.9, 2, '.', '');
-            $delivery_cost = $area_code != '0' ? number_format($area_code + 3.1, 2, '.', ''): '0';
+            $necropsy_cost = number_format($this->calc_necropsy($weight), 2, '.', '');
+            $cremation_cost = number_format($this->calc_cremation($weight), 2, '.', '');
+            $delivery_cost = $area_code != '0' ? number_format($this->calc_delivery($area_code),2, '.', ''): '0';
             $total_cost = number_format($delivery_cost + $necropsy_cost + $cremation_cost, 2, '.', '');
 
+            
             
             // Set Calculations into Session
             $calculations = array(
@@ -87,7 +88,7 @@ class Estimate_Controller extends CI_Controller {
 
             // STORE IN TXT
             $this->Record->add_record($this->session->userdata('hospital'),$this->session->userdata('estimate'),'estimates.txt');
-            echo nl2br("\n CALCULATION  BEGUN \n");
+            //echo nl2br("\n CALCULATION  BEGUN \n");
 
         } else {
 
@@ -98,15 +99,16 @@ class Estimate_Controller extends CI_Controller {
             );
             // Set Errors
             $this->session->set_flashdata('errors', $errors);
-            echo nl2br("\n VALIDATION ERROR \n");
+            //echo nl2br("\n VALIDATION ERROR \n");
 
         };
 
-        // PRINT FUNCTIONS
-        echo 'CALCULATE FUNCTION';
-        $this->array_helper->printArr('ESTIMATE', $this->session->userdata('estimate'));
-        $this->array_helper->printArr('HOSPITAL', $this->session->userdata('hospital'));
-        $this->array_helper->printArr('POST', $this->input->post());
+        //PRINT FUNCTION
+        //echo 'CALCULATE FUNCTION';
+        //echo $this->calc_necropsy($weight);
+        // $this->array_helper->printArr('ESTIMATE', $this->session->userdata('estimate'));
+        //$this->array_helper->printArr('HOSPITAL', $this->session->userdata('hospital'));
+        //$this->array_helper->printArr('POST', $this->input->post());
     
         // Return to main page
         redirect('/');
@@ -176,6 +178,87 @@ class Estimate_Controller extends CI_Controller {
 
         redirect('/');
     }
+
+
+    //************************************************* */
+    // CALCULATE NECROPSY COST
+    //************************************************* */
+    
+    public function calc_necropsy($weight){
+        $total = 0;
+        switch ($weight)
+        {
+            case $weight <= 9:                      $total = 1380; break;
+            case $weight >= 10 && $weight <= 39:    $total = 1525; break;
+            case $weight >= 40 && $weight <= 59:    $total = 1670; break;
+            case $weight >= 60 && $weight <= 89:    $total = 2035; break;
+            case $weight >= 90 && $weight <= 114:   $total = 2250; break;
+            case $weight > 114:                     $total = 2616; break;  
+            default:
+                echo 'Oops, there was a problem with your weight.';
+                $total = -1;
+                break;         
+        }      
+        return $total;         
+    }
+
+    //************************************************* */
+    // CALCULATE CREMTION COST
+    //************************************************* */
+    
+    public function calc_cremation($weight){
+        $total = 0;
+        switch ($weight)
+        {
+            case $weight >= 0 && $weight <= 10:         $total = 165; break;
+            case $weight >= 10.1 && $weight <= 20:      $total = 180; break;
+            case $weight >= 20.1 && $weight <= 50:      $total = 200; break;
+            case $weight >= 50.1 && $weight <= 70:      $total = 235; break;
+            case $weight >= 70.1 && $weight <= 150:     $total = 255; break;
+            case $weight >= 150.1 && $weight <= 250:    $total = 255; break; 
+            default:
+                echo 'Oops, there was a problem with your weight.';
+                $total = -1;
+                break;         
+        }      
+        return $total;         
+    }
+
+
+    //************************************************* */
+    // CALCULATE DELIVERY COST
+    //************************************************* */
+    
+    public function calc_delivery($area_code){
+        $total = 0;
+        switch ($area_code)
+        {
+            case 818: $total = 165; break;
+            case 747: $total = 165; break;
+            case 310: $total = 150; break;
+            case 626: $total = 150; break;
+            case 323: $total = 141; break;
+            case 213: $total = 141; break;
+            case 714: $total = 125; break;
+            case 949: $total = 125; break;
+            case 951: $total = 150; break;
+            case 909: $total = 160; break;
+            case 760: $total = 250; break;
+            case 562: $total = 140; break;
+            case 619: $total = 260; break;
+            case 858: $total = 248; break;
+            case 424: $total = 150; break;
+            case 0:   $total = 0; break;
+            default:
+                echo 'Oops, there was a problem with your area_code.';
+                $total = -1;
+                break;         
+        }      
+        //echo $area_code;
+        return $total;         
+    }
+
+
 
     //************************************************* */
     // MAIN CALCULATOR VIEW 
--- Lamp/application/models/Record.php
+++ Lamp/application/models/Record.php
@@ -162,10 +162,25 @@ class Record extends CI_Model {
 
     public function add_record( $hospital, $estimate, $filename) 
     {
-        $date = date("m-d-Y");
-        $time = date("h:i:s A");
-        $str = 
+        $interval = 100;
+        $this->session->set_userdata('last_total', 0);
 
+        $time = date("h:i:s A");
+
+        if(array_key_exists('last_time',$this->session->userdata))
+        {
+            //echo nl2br("\nKEY EXISTS\n");
+            $last_time = date_create($this->session->userdata('last_time'));
+            $new_time = date_create($time);
+            $interval = date_diff($last_time,$new_time)->format("%s");
+
+        }
+        
+        if($interval > 30 && $estimate['total_cost'] != $this->session->userdata('last_total')){
+            //echo nl2br("\nADDING TO TEXT FILE...\n");
+            $date = date("m-d-Y");
+            $str =
+    
 "************************************************
 Date of Quote:           " . $date . "
 Time of Quote:           " . $time . "
@@ -179,16 +194,24 @@ Cremation:               " . $estimate['cremation_cost'] . "
 Full Necropsy Total:     " . $estimate['total_cost'] . "
 ************************************************
 ";
+            
+            file_put_contents($filename ,$str,FILE_APPEND);
+    
+            $this->session->set_userdata('last_time', date('h:i:s A'));
+            $this->session->set_userdata('last_total', $estimate['total_cost']);
         
-        file_put_contents($filename ,$str,FILE_APPEND);
-    
+            
+        }
 
-    // echo 'ADD RECORD FUNCTION';
-    // $this->array_helper->printArr('ESTIMATE', $estimate);
-    // $this->array_helper->printArr('HOSPITAL', $hospital);
-    // echo "FILENAME :" . $filename;
+    // echo nl2br("\nADD RECORD FUNCTION\n");
+    // echo nl2br($this->session->userdata('last_total'). "\n");
+    // echo nl2br($estimate['total_cost'] . "\n");
+    // echo nl2br($interval . "\n");
+   // $this->array_helper->printArr('ESTIMATE', $estimate);
+   // $this->array_helper->printArr('HOSPITAL', $hospital);
+    //echo "FILENAME :" . $filename;
     }
-    
+
 
 }
 
